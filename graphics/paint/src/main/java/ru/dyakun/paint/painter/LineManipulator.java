206bfe60fceb951bdd03f58fbca06bfedd96955e
Ilya Dyakun <idyakun0@gmail.com>
2024-02-11 19:54:48 +0700

    Bresenham

--- graphics/paint/src/main/java/ru/dyakun/paint/painter/LineManipulator.java
+++ graphics/paint/src/main/java/ru/dyakun/paint/painter/LineManipulator.java
@@ -34,12 +34,12 @@ public class LineManipulator extends Manipulator {
         } else {
             int x = e.getX();
             int y = e.getY();
+            Graphics2D g = (Graphics2D) canvas.getImage().getGraphics();
+            g.setStroke(new BasicStroke(strokeWidth.getVal()));
+            g.setColor(canvas.getColor());
             if(strokeWidth.getVal() == 1) {
-                drawBresenhamLine(canvas, x0, y0, x, y);
+                drawBresenhamLine(g, x0, y0, x, y);
             } else {
-                Graphics2D g = (Graphics2D) canvas.getImage().getGraphics();
-                g.setStroke(new BasicStroke(strokeWidth.getVal()));
-                g.setColor(canvas.getColor());
                 g.drawLine(x0, y0, x, y);
             }
             x0 = x;
@@ -48,8 +48,47 @@ public class LineManipulator extends Manipulator {
         }
     }
 
-    private void drawBresenhamLine(Canvas canvas, int x0, int y0, int x1, int y1) {
-
+    public static void drawBresenhamLine(Graphics g, int x0, int y0, int x1, int y1) {
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+        int signX = Integer.compare(dx, 0);
+        int signY = Integer.compare(dy, 0);
+        if (dx < 0) {
+            dx = -dx;
+        }
+        if (dy < 0) {
+            dy = -dy;
+        }
+        int pdx, pdy, es, el;
+        if (dx > dy) {
+            pdx = signX;
+            pdy = 0;
+            es = dy;
+            el = dx;
+        }
+        else {
+            pdx = 0;
+            pdy = signY;
+            es = dx;
+            el = dy;
+        }
+        int x = x0;
+        int y = y0;
+        int err = el / 2;
+        g.drawLine(x, y, x, y);
+        for (int i = 0; i < el; i++) {
+            err -= es;
+            if (err < 0) {
+                err += el;
+                x += signX;
+                y += signY;
+            }
+            else {
+                x += pdx;
+                y += pdy;
+            }
+            g.drawLine(x, y, x, y);
+        }
     }
 
 }
